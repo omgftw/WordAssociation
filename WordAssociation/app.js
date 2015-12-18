b6b3fe214655e8277e8acee17e2d7d9b6a8d5b96
Josh <firstmilkman@gmail.com>
2015-12-18 01:37:45 -0500

    Initial Commit

---
+++ WordAssociation/app.js
@@ -0,0 +1,57 @@
+﻿
+/**
+ * Module dependencies.
+ */
+
+var express = require("express");
+var routes = require("./routes");
+var path = require("path");
+
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+// all environments
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.use(express.favicon());
+app.use(express.logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require("stylus").middleware(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+// development only
+if ("development" == app.get("env")) {
+    app.use(express.errorHandler());
+}
+
+//socket.io
+io.on("connection", function(socket) {
+    console.log("Client has connected");
+    socket.username = "Name";
+
+    socket.on("chat", function (data) {
+        var output = {
+            message: data,
+            sender: socket.username
+        };
+        socket.broadcast.emit("chat", output);
+    });
+
+    socket.on("disconnect", function() {
+        console.log("Client has disconnected");
+    });
+});
+
+app.get("/", routes.index);
+app.get("/about", routes.about);
+app.get("/contact", routes.contact);
+app.get("/wordassociation", routes.wordassociation);
+
+http.listen(app.get("port"), function () {
+    console.log("Express server listening on port " + app.get("port"));
+});
---
+++ WordAssociation/public/javascripts/Angular/wordAssociationController.js
@@ -0,0 +1,76 @@
+﻿(function() {
+
+    function ChatMessage(message, sender) {
+        this.sender = sender || "Name";
+        this.timestamp = new Date().toLocaleTimeString();
+        this.message = message || "";
+    };
+
+    function wordAssociationController($scope, $q, $timeout) {
+        var vm = this;
+        vm.chatInput = "";
+        vm.chatMessages = [];
+        vm.seed = 1;
+        vm.cards = [];
+        vm.words = ["ball", "bat", "bed", "book", "boy", "bun", "can", "cake", "cap", "car", "cat", "cow", "cub", "cup", "dad", "day", "dog", "doll", "dust", "fan", "feet", "girl", "gun", "hall", "hat", "hen", "jar", "kite", "man", "map", "men", "mom", "pan", "pet", "pie", "pig", "pot", "rat", "son", "sun", "toe", "tub", "van", "apple", "arm", "banana", "bike", "bird", "book", "chin", "clam", "class", "clover", "club", "corn", "crayon", "crow", "crown", "crowd", "crib", "desk", "dime", "dirt", "dress", "fang ", "field", "flag", "flower", "fog", "game", "heat", "hill", "home", "horn", "hose", "joke", "juice", "kite", "lake", "maid", "mask", "mice", "milk", "mint", "meal", "meat", "moon", "mother", "morning", "name", "nest", "nose", "pear", "pen", "pencil", "plant", "rain", "river", "road", "rock", "room", "rose", "seed", "shape", "shoe", "shop", "show", "sink", "snail", "snake", "snow", "soda", "sofa", "star", "step", "stew", "stove", "straw", "string", "summer", "swing", "table", "tank", "team", "tent", "test", "toes", "tree", "vest", "water", "wing", "winter", "woman", "women", "alarm", "animal", "aunt", "bait", "balloon", "bath", "bead", "beam", "bean", "bedroom", "boot", "bread", "brick", "brother", "camp", "chicken", "children", "crook", "deer", "dock", "doctor", "downtown", "drum", "dust", "eye", "family", "father", "fight", "flesh", "food", "frog", "goose", "grade", "grandfather", "grandmother", "grape", "grass", "hook", "horse", "jail", "jam", "kiss", "kitten", "light", "loaf", "lock", "lunch", "lunchroom", "meal", "mother", "notebook", "owl", "pail", "parent", "park", "plot", "rabbit", "rake", "robin", "sack", "sail", "scale", "sea", "sister", "soap", "song", "spark", "space", "spoon", "spot", "spy", "summer", "tiger", "toad", "town", "trail", "tramp", "tray", "trick", "trip", "uncle", "vase", "winter", "water", "week", "wheel", "wish", "wool", "yard", "zebra"];
+
+        vm.rng = function (min, max) {
+            var x = Math.sin(vm.seed++) * 10000;
+            var rand = x - Math.floor(x);
+
+            return Math.floor((rand * max) + min);
+        }
+
+        vm.initCards = function() {
+            for (var i = 0; i < 25; i++) {
+                var index = -1;
+                var found = 1;
+                while (found !== -1) {
+                    index = vm.rng(0, vm.words.length - 1);
+                    found = vm.cards.indexOf(vm.words[index]);
+                }
+                vm.cards.push(vm.words[index]);
+            }
+        };
+
+        vm.initCards();
+
+        vm.socket = io();
+
+        vm.socket.on("chat", function (data) {
+            var message = new ChatMessage(obfusChat.obfuscate(data.message, vm.seed), data.sender);
+            vm.chatMessages.push(message);
+            $scope.$apply();
+        });
+
+        vm.sendChat = function () {
+            var deferred = $q.defer();
+
+            var message = new ChatMessage(vm.chatInput, "You");
+            vm.chatMessages.push(message);
+            vm.socket.emit("chat", obfusChat.obfuscate(vm.chatInput, vm.seed));
+            vm.chatInput = "";
+        }
+
+        vm.inputKeyPress = function(event) {
+            if (event.which === 13) {
+                vm.sendChat();
+            }
+        }
+
+        vm.chatWindowScrollToBottom = function () {
+            var chatWindow = $("#chat-window");
+            chatWindow.scrollTop(chatWindow[0].scrollHeight);
+        };
+
+        //scroll to the bottom of the chat box when a new message is added
+        $scope.$watch(function() { return vm.chatMessages.length },
+            function () {
+                $timeout(function() {
+                    vm.chatWindowScrollToBottom();
+                }, 0);
+        });
+    }
+
+    angular.module("app").controller("wordAssociationController", ["$scope", "$q", "$timeout", wordAssociationController]);
+})();
